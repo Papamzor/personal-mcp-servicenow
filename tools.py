14618d9fdbf937d117a2cbe0341704ec69ebd014
Papamzor <89700643+Papamzor@users.noreply.github.com>
2025-07-15 10:02:10 +0200

    Added dividers on tools

--- tools.py
+++ tools.py
@@ -11,11 +11,13 @@ mcp = FastMCP("mcpnowsimilarity", version="1.0.0", description="MCP Now Similari
 mcp.tool()(nowtest)
 mcp.tool()(nowtestauth)
 mcp.tool()(nowtestauthInput)
+# Incident tools
 mcp.tool()(similarincidentsfortext)
 mcp.tool()(getshortdescforincident)
 mcp.tool()(similarincidentsforincident)
+mcp.tool()(getincidentdetails)
+# Change tools
 mcp.tool()(changesfortext)
 mcp.tool()(getshortdescforchange)
 mcp.tool()(similarchangefortext)
-mcp.tool()(getincidentdetails)
 mcp.tool()(getchangedetails)
