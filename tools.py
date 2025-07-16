a929fdf0c8fbbe714acf3fe42de3dc89c2d8dc0e
Papamzor <89700643+Papamzor@users.noreply.github.com>
2025-07-16 16:00:55 +0200

    Moved kb_tools to Table_tools

--- tools.py
+++ tools.py
@@ -3,6 +3,7 @@ from mcp.server.fastmcp import FastMCP
 from Table_Tools.incident_tools import similarincidentsfortext, getshortdescforincident, similarincidentsforincident, getincidentdetails
 from Table_Tools.change_tools import similarchangesfortext, getshortdescforchange, similarchangesforchange, getchangedetails
 from Table_Tools.ur_tools import similarURfortext, getshortdescforUR, similarURsforUR, getURdetails
+from Table_Tools.kb_tools import similar_knowledge_for_text, get_knowledge_details
 from Table_Tools.table_tools import nowtestauth, nowtestauthInput
 from utility_tools import nowtest
 
@@ -27,3 +28,6 @@ mcp.tool()(similarchangesfortext)
 mcp.tool()(getshortdescforchange)
 mcp.tool()(similarchangesforchange)
 mcp.tool()(getchangedetails)
+# Knowledge Base tools
+mcp.tool()(similar_knowledge_for_text)
+mcp.tool()(get_knowledge_details)
